b1a1f43245a48df0ae61f8d0033800e4feeed28f
CyrusJavan <cyrus.javan@sjsu.edu>
2019-01-28 16:39:10 -0800

    Added portfolio views

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const exphbs = require('express-handlebars')
 const port = process.env.PORT || 5000;
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'vidjot/public')));
 
 app.use('/vidjot', require('./vidjot/app').app);
+
+//Handlebars middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  layoutsDir: 'views/portfolio/layouts',
+  partialsDir: 'views/portfolio/partials'
+}));
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
   res.render('index')
