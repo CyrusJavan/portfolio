5c3f3b853c9400d9cb3d11e858cdf773c808fc69
CyrusJavan <cyrus.javan@sjsu.edu>
2019-02-05 14:28:11 -0800

    Working on OAuth Session Heroku fix

--- storybooks/app.js
+++ storybooks/app.js
@@ -75,7 +75,7 @@ app.use(methodOverride('_method'))
 
 // Allow CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://www.cyrusjavan.com')
+  res.header('Access-Control-Allow-Origin', 'www.cyrusjavan.com')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-AUTHENTICATION, X-IP, Content-Type, Accept')
   res.header('Access-Control-Allow-Credentials', true)
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
