3954505ed4085209d75e24770cd8684369d162a1
CyrusJavan <cyrus.javan@sjsu.edu>
2019-02-05 13:47:28 -0800

    Working on OAuth Session Heroku fix

--- storybooks/app.js
+++ storybooks/app.js
@@ -89,7 +89,6 @@ app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false,
-  proxy: true,
   store: new MongoStore({ mongooseConnection: mongoose.connection }),
   cookie: {
     secure: false,
