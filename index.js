9902db9453abb63f0276ca4694bcd3f7c156c1a9
CyrusJavan <cyrus.javan@sjsu.edu>
2019-02-07 12:29:50 -0800

    Changed to port 80

--- index.js
+++ index.js
@@ -65,9 +65,9 @@ app.get('*', function (req, res, next) {
 
 //const PORT = process.env.PORT || 8080;
 //const HOST = process.env.HOST || '0.0.0.0';
-const PORT = 8080;
+const PORT = 80;
 const HOST = '0.0.0.0';
 
 app.listen(PORT, HOST, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(`Server started at http://${HOST}:${PORT}`);
 });
