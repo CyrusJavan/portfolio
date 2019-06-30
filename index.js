c1ca740d5c0c73b335eea62b80ce597393feba68
CyrusJavan <cyrus.javan@sjsu.edu>
2019-06-29 21:51:53 -0700

    added sass

--- index.js
+++ index.js
@@ -65,8 +65,8 @@ app.get('*', function (req, res, next) {
 
 //const PORT = process.env.PORT || 8080;
 //const HOST = process.env.HOST || '0.0.0.0';
-const PORT = 80;
-const HOST = '0.0.0.0';
+const PORT = 8080;
+const HOST = '127.0.0.1';
 
 app.listen(PORT, HOST, () => {
   console.log(`Server started at http://${HOST}:${PORT}`);
